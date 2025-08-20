0d2c6c134fd486a922ec60c14613a5f02f76c817
Isioma Ogbuehi <isiomaogbuehi@gmail.com>
2025-08-20 12:10:26 +0100

    product_minus_self

--- leetcode/arrays/product_minus_self.ts
+++ leetcode/arrays/product_minus_self.ts
@@ -9,36 +9,21 @@ Difficulty: Medium
  */
 
 const productExceptSelf = (nums: number[]): number[] => {
-    let outputArray: number[] = Array(nums.length).fill(1)
-    /* 
-    Populate output array with prefix values: Move one step forward with default value of
-    1 in first array. 
-    */
-   let prefixValue = 1
-    for(let i=0; i<nums.length; i++) {
-        // if(i > 0) {
-        //     prefixValue = prefixValue * nums[i]
-        // }
-        // outputArray.push(prefixValue)
-        outputArray[i] = prefixValue
-        prefixValue *= nums[i]
-    }
-    console.log(outputArray)
+    let outputArray = Array(nums.length).fill(1)
 
-    let postfixValue = 1
-    // let postfix = 1
-    for (let i = nums.length - 1; i >= 0; i--) {
-      // if(i < nums.length - 1) {
-      //     postfix = i + 1
-      //     outputArray[i] = postfixValue * outputArray[i]
-      //     postfixValue = nums[i] * postfixValue
-      // } else {
-      //     postfixValue = nums[nums.length - 1]
-      // }
-      outputArray[i] *= postfixValue
-      postfixValue *= nums[i]
+    let prefix = 1
+    for(let i=0; i<nums.length; i++) {
+      outputArray[i] = prefix
+      prefix *= nums[i]
     }
-    return outputArray
+
+    let suffix = 1
+    for(let i=nums.length - 1; i>=0; i--) {
+      outputArray[i] *= suffix
+      suffix *= nums[i]
+    }
+
+    return outputArray.map(x => Object.is(x, -0) ? 0 : x)
 }
 
-console.log(productExceptSelf([-1, 1, 0, -3, 3]))
+console.log(productExceptSelf([-1,1,0,-3,3]))
